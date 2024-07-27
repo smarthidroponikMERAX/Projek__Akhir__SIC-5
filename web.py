63033e9151c30945595299563d8c067440b03e48
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-27 19:32:43 +0700

    Update web.py

--- web.py
+++ web.py
@@ -7,8 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import convert_csv.py
-
+import convert_csv
 
 st.set_page_config(page_title='Smart Hidroponik', layout="wide", page_icon="🍀")
 
