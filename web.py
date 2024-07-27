b7bb864d014a677daec765caad038153b354f4af
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-27 21:16:05 +0700

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 #"""============================ LIBRARY ===================================="""
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 from pymongo import MongoClient
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
