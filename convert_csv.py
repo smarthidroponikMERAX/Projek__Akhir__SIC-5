5572ae69775b5f783004c7bc03dbd47a3fac4475
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-27 18:57:10 +0700

    Update convert_csv.py

--- convert_csv.py
+++ convert_csv.py
@@ -3,50 +3,51 @@ import pandas as pd
 from pymongo import MongoClient
 import io
 
-def database:
-    client = MongoClient('mongodb+srv://SmartHidroponik:MERA_X@smarthidroponik.hdetbis.mongodb.net/?retryWrites=true&w=majority&appName=SmartHidroponik')
-    database = client['Smart_Hidroponik']
-    koleksi = database['Sensor']
-    data = list(koleksi.find())
-    return data
-
-def convert_to_csv(df):
-    return data_frame.to_csv(index=False).encode('utf-8')
-
-
-st.title('Unduh Sampel Data MongoDB sebagai CSV')
-
-
-data = database()
-
-
-data_frame = pd.DataFrame(data)
-
-
-if '_id' in df.columns:
-    df.drop(columns=['_id'], inplace=True)
-
-total_samples = len(df)
-
-
-slide_data_sampel = st.slider("Number of samples", min_value=1, max_value=total_samples, value=min(10, total_samples))
-
-
-generate_button = st.button("Generate CSV")
-
-if generate_button:
-
-    df_sample = df.head(num_samples)
-    
-
-    data_csv = convert_to_csv(df_sample)
-    
-
-    data_byte = io.BytesIO(data_csv)
-    st.download_button(
-        label="Unduh CSV",
-        data=data_byte,
-        file_name='data_sample_smarthidroponik.csv',
-        mime='text/csv'
-    )
-    st.success(f"Data sampel ({slide_data_sampel} baris) berhasil, siap diunduh sebagai CSV!")
+def show():
+        def database:
+            client = MongoClient('mongodb+srv://SmartHidroponik:MERA_X@smarthidroponik.hdetbis.mongodb.net/?retryWrites=true&w=majority&appName=SmartHidroponik')
+            database = client['Smart_Hidroponik']
+            koleksi = database['Sensor']
+            data = list(koleksi.find())
+            return data
+        
+        def convert_to_csv(df):
+            return data_frame.to_csv(index=False).encode('utf-8')
+        
+        
+        st.title('Unduh Sampel Data MongoDB sebagai CSV')
+        
+        
+        data = database()
+        
+        
+        data_frame = pd.DataFrame(data)
+        
+        
+        if '_id' in df.columns:
+            df.drop(columns=['_id'], inplace=True)
+        
+        total_samples = len(df)
+        
+        
+        slide_data_sampel = st.slider("Number of samples", min_value=1, max_value=total_samples, value=min(10, total_samples))
+        
+        
+        generate_button = st.button("Generate CSV")
+        
+        if generate_button:
+        
+            df_sample = df.head(num_samples)
+            
+        
+            data_csv = convert_to_csv(df_sample)
+            
+        
+            data_byte = io.BytesIO(data_csv)
+            st.download_button(
+                label="Unduh CSV",
+                data=data_byte,
+                file_name='data_sample_smarthidroponik.csv',
+                mime='text/csv'
+            )
+            st.success(f"Data sampel ({slide_data_sampel} baris) berhasil, siap diunduh sebagai CSV!")
