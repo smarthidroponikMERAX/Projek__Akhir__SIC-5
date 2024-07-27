9e325504015eab7a68b73747ece37f88fe875a5e
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-27 19:14:31 +0700

    Update web.py

--- web.py
+++ web.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import convert_csv
 
 
 
