e0b0ef503b7459e33e56de4ec1ca0d04feb2cd62
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-27 19:51:01 +0700

    Update web.py

--- web.py
+++ web.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import convert_csv
+#import convert_csv
 
 st.set_page_config(page_title='Smart Hidroponik', layout="wide", page_icon="🍀")
 
@@ -426,11 +426,11 @@ st.write(f"Recall (Nutrisi): {recall_nutrisi:.2f}")
 st.write(f"F1 Score (Nutrisi): {f1_nutrisi:.2f}")
 
 
-if 'page' not in st.session_state:
-    st.session_state['page'] = 'convert_csv'
-# Fungsi untuk mengatur navigasi
-def navigate_to(page):
-    st.session_state['page'] = page
-st.sidebar.button("Konversi ke Csv", on_click=navigate_to, args=("convert_csv",))
-if st.session_state['page'] == 'convert_csv':
-    convert_csv.show()
+# if 'page' not in st.session_state:
+#     st.session_state['page'] = 'convert_csv'
+# # Fungsi untuk mengatur navigasi
+# def navigate_to(page):
+#     st.session_state['page'] = page
+# st.sidebar.button("Konversi ke Csv", on_click=navigate_to, args=("convert_csv",))
+# if st.session_state['page'] == 'convert_csv':
+#     convert_csv.show()
